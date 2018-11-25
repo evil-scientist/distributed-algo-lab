b67a6c8a154122efd257f2eb7ab8116568bb471e
evil-scientist <suryansh@evilscientist.cc>
2018-11-25 09:00:13 +0100

    Working Codegit status -s

--- Code/lab2/Component.java
+++ Code/lab2/Component.java
@@ -3,38 +3,39 @@
  * Describes the procedures for a single process P
  * Used to create objects which are single process in system
  */
-package chandyLamport;
 import java.rmi.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.net.MalformedURLException;
 import java.rmi.server.UnicastRemoteObject;
-//import java.net.MalformedURLException;
-//import java.rmi.RemoteException;
 
-public class Component extends UnicastRemoteObject implements RMI
+public class Component extends UnicastRemoteObject implements RMI_Interface
 {
-	private int processID; // Unique ID for each Process Object
-	private int mystate = 0; // State of Process P
-	int channelID; // Identify channel c
-	int channel_number; // Total number of channels (must be)= number of processes
-	// Array list for each channel which stores the value of Channel c
-	
+	private static final long serialVersionUID = 1L; // For RMI
+	int processID; // Unique ID for each Process Object
+	int total_proc; // Total number of channels = number of processes
+	/*
+	 * EACH PROCESS HAS 2 SETS OF CHANNELS
+	 * 
+	 */
+	List<String> QBuffer = new ArrayList<>();// Array list for each channel to store the value of Channel c
+	List<String> myState = new ArrayList<>(); // Array list for each Process to store its state
 	private boolean recording_local_state = false; // Check if recording self state
 	char marker = '#'; // Send # as marker
-	char message; // Actual message sent over any channel (convert to int and add to mystate)
+	String message; // Actual message sent over any channel
 	
-	private static final long serialVersionUID = 1L; // for RMI
-	
-	public Component(int procID, int state) throws RemoteException
+	public Component(int procID, int total_process) throws RemoteException
 	{
 		this.processID=procID;
-		this.mystate=state;
-		try {
-		    java.rmi.Naming.bind("rmi://localhost:1099/process"+processID, this);
-		    System.err.println("Process ready");
-			} 
-		catch (Exception e) {
-		    System.err.println("RMI Binding exception: " + e.toString());
-		    e.printStackTrace();
-			}
+		this.total_proc=total_process;
+		QBuffer.add(0,"NA");
+		myState.add(0,"NA");
+		for (int i=1; i<=total_process; i++)
+		{
+		    QBuffer.add(i,"");
+		    myState.add(i,"");
+		}
+		
 	}
 
 	// Record Local State
@@ -42,51 +43,81 @@ public class Component extends UnicastRemoteObject implements RMI
 	{
 		// DO SOMETHING TO RECORD LOCAL STATE
 		// Maybe save value of some variable into a different variable
-		System.out.println("Process " + processID + " recorded its local state: " + mystate);
+		System.out.println("Process " + processID + " recorded its local state: " + myState);
 		
 		recording_local_state = true;
 		
-		for (int i =0; i < channel_number; i ++) {
-			send(channel_number,"#"); // Send marker along every channel
+		for (int i =1; i <= total_proc; i ++) {
+			if (i == processID) {
+				continue;
+			}
+			send(i,"#"); // Send marker along every channel
 		}
-		
-		for (int i =0; i < channel_number; i ++) {
-			// Create message Buffer Q(c) for each channel 
-		}
+		/*
+		 *for (int i =0; i < total_proc; i ++) {
+		 *	// Create message Buffer Q(c) for each channel 
+		 *}
+		 */
 	}
 	
-	public void receive(String message){
-		// Receive message along channel
-		switch(message.charAt(0)){
-			// Received a marker
-			case '#':   
-						System.out.println("Marker received from Process "+ processID +" along Channel: "+channelID);
-						if(!recording_local_state) {
+	public void receive(int senderID,String message){
+		/*
+		 * MESSAGE HAVE THE FOLLOWING FORMAT:
+		 * <type><sender process>
+		 * ex: m1 [M from 1]
+		 */
+		// Received something along channel from Process with processID = senderID 
+		//CHECK IF MARKER OR MESSAGE
+		switch(message.charAt(0))
+		{
+			case '#':  
+					System.out.println("Marker received from Process "+ senderID +" along Channel: "+senderID);
+					// CHECK IF ALREADY RECORDED STATE
+					if(!recording_local_state) 
+					{
 						// Record state of channel ID as 0 (empty)
-								record_local_state();
-						}
-						else {
-								// Contents of Q(c) == channelState;
-						}
-						break;
+						record_local_state();
+					}
+					else 
+					{
+						 System.out.println("The state of Channel "+Integer.toString(senderID)+" is : "+QBuffer.get(senderID)); // Contents of Q(c) == channelState(c);
+					}
+					break;						
+			
+			case 'm':
+					System.out.println("Message: "+message+" received from Process "+ senderID +" along Channel: "+senderID);							
+					if(recording_local_state) 
+					{
+						// ADD MESSAGE TO QBUFFER (replace the string of messages 
+						// present at SenderID location in buffer to include latest message
+						String update = QBuffer.get(senderID);
+						update = update+","+message;
+						QBuffer.add(senderID,update);
+					}
+					else {
+						// Replace message string at senderId location 
+						// in myState Buffer to reflect latest received message from Process [senderID]
+						myState.add(senderID,message); 
+					}
+					break;
 			default:
-						System.out.println("Message: "+message+"received from Process "+ processID +" along Channel: "+channelID);							
-						int value = Integer.parseInt(message); 
-						mystate = mystate + value;
-						break;
-			}			
+					System.out.println("Message type unknown");
+					break;
+		}			
 	}
 
-	// Method which sends a message along a single channel (can send marker)
-	public void send(int procID, String message){
-		// Send message along Channel ID
-		try {	
-				RMI proc = (RMI) java.rmi.Naming.lookup("rmi://localhost:1099/process"+procID);
-				proc.receive(message);
-		}
-		catch (Exception e) {
-		    System.err.println("Lookup exception: " + e.toString());
-		    e.printStackTrace();
+	// Method which sends a message/marker along a single channel
+	public void send(int receiverID, String message){
+		// Send message to Process with processID = receiverID
+		try
+		{
+			RMI_Interface p =(RMI_Interface)java.rmi.Naming.lookup("rmi://localhost/process"+receiverID);
+			myState.add(processID,message); // ADD LAST SENT MESSAGE TO OWN STATE
+			p.receive(processID,message);
+		}	
+		catch (RemoteException | NotBoundException | MalformedURLException e)
+		{	
+			System.out.println("Error in sending");
 		}
 	}
 }
--- Code/lab2/Main.java
+++ Code/lab2/Main.java
@@ -2,17 +2,57 @@
  * Main Class that creates all processes that run on a (single) host.
  * Used to create objects of Component class which are processes in system.
  */
-package chandyLamport;
+
+import java.net.MalformedURLException;
 import java.rmi.*;
+import java.io.IOException;
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int numproc; //number of processes
+	static int proc_id; //Process identifier
+	
+	public static void main(String[] args)throws RemoteException, AlreadyBoundException
+	{
 		// Specify number of Processes to be created using argument
 		// Set up number of channels for each process
-
+		numproc = Integer.parseInt(args[0]);
+		proc_id = Integer.parseInt(args[1]);
+		Component proc  = new Component(proc_id,numproc);  
+		try 
+		{
+		java.rmi.Naming.bind("rmi://localhost/process"+proc_id,proc);
+		}
+		catch(MalformedURLException e)
+		{
+			System.out.println("Malformed URL");
+		}
+		System.out.println("Press \"ENTER\" to continue...");
+        try {
+        System.in.read();
+        } 
+        catch (IOException e) 
+        {
+        e.printStackTrace();
+        }
+		Thread t1 = new Thread(() ->{
+			for (int i =1; i<= 5; i++) 
+			{
+				String message = 'm'+Integer.toString(i);	
+				for (int j = 1; j <= numproc; j ++) 
+				{
+					if (j == proc_id) 
+					{
+					continue;
+					}
+				proc.send(j,message); // Send marker along every channel
+				
+				}
+				if(proc_id==1)
+				{
+					proc.record_local_state();
+				}
+			}
+		});
+		t1.start();
 	}
 }
